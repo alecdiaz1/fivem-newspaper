244881fcb98cca8499f40bbfae6364561f042b6e
Alec Diaz <alecdiaz1@yahoo.com>
2021-04-09 22:30:06 -0700

    Move testing newspapers to file

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -9,39 +9,7 @@ import NewspaperListItem from '../components/NewspaperListItem'
 import { useVisibility } from '../core/hooks/useVisibility'
 import { useCoreService } from '../core/hooks/useCoreService'
 import { Typography } from '@material-ui/core'
-
-const SAMPLE_NEWSPAPERS = [
-  {
-    date: '4/9/2021',
-    preview: 'article 1 preview',
-    articles: [
-      {
-        title: 'article 1',
-        body: 'body 1',
-      },
-    ],
-  },
-  {
-    date: '4/10/2021',
-    preview: 'article 2 preview',
-    articles: [
-      {
-        title: 'article 2',
-        body: 'body 2',
-      },
-    ],
-  },
-  {
-    date: '4/11/2021',
-    preview: 'article 3 preview',
-    articles: [
-      {
-        title: 'article 3',
-        body: 'body 3',
-      },
-    ],
-  },
-]
+import { SAMPLE_NEWSPAPERS } from './../TEST_NEWSPAPERS';
 
 function App() {
   useCoreService()
