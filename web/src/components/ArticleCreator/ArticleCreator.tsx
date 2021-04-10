5576d80c29066ab6977a95f0dc056253706d7b52
Alec Diaz <alecdiaz1@yahoo.com>
2021-04-09 18:25:43 -0700

    Fix styling on Create Newspaper

--- web/src/components/ArticleCreator/ArticleCreator.tsx
+++ web/src/components/ArticleCreator/ArticleCreator.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import styled from 'styled-components'
+import { Grid } from '@material-ui/core'
 
 const FormContainer = styled.div`
   display: flex;
@@ -28,34 +29,40 @@ function ArticleCreator(props: { onSubmit: (article: Article) => void }) {
   }
 
   return (
-    <FormContainer>
-      <TextField
-        id="outlined-helperText"
-        label="Title"
-        defaultValue="Dog Fetches Bone"
-        variant="outlined"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <TextField
-        id="body"
-        name="body"
-        label="Body"
-        placeholder="Local dog Sparky, recently fetched a large bone."
-        multiline
-        variant="outlined"
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-      />
-      <Button
-        type="submit"
-        onClick={() => onSubmit()}
-        variant="contained"
-        color="primary"
-      >
-        Add Article
-      </Button>
-    </FormContainer>
+    <Grid container direction="column" alignItems="center" spacing={2}>
+      <Grid item>
+        <TextField
+          id="outlined-helperText"
+          label="Title"
+          defaultValue="Dog Fetches Bone"
+          variant="outlined"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </Grid>
+      <Grid item>
+        <TextField
+          id="body"
+          name="body"
+          label="Body"
+          placeholder="Local dog Sparky, recently fetched a large bone."
+          multiline
+          variant="outlined"
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </Grid>
+      <Grid item>
+        <Button
+          type="submit"
+          onClick={() => onSubmit()}
+          variant="contained"
+          color="primary"
+        >
+          Add Article
+        </Button>
+      </Grid>
+    </Grid>
   )
 }
 
--- web/src/pages/CreateNewspaper.tsx
+++ web/src/pages/CreateNewspaper.tsx
@@ -3,6 +3,11 @@ import Button from '@material-ui/core/Button'
 import { Link } from 'react-router-dom'
 import ArticleCreator from '../components/ArticleCreator/ArticleCreator'
 import styled from 'styled-components'
+import { Divider, Grid } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const Article = styled.div`
   padding: 10px;
@@ -23,24 +28,41 @@ function CreateNewspaper() {
   }
 
   return (
-    <div>
-      <Link to="/">
-        <Button variant="contained" color="primary">
-          Back
-        </Button>
-      </Link>
-      <ArticleCreator
-        onSubmit={(newArticle: Article) => addNewArticle(newArticle)}
-      />
-      <div>
-        {articles.map((article) => (
-          <Article>
-            <h2>Title: {article.title}</h2>
-            <p>{article.body}</p>
-          </Article>
-        ))}
-      </div>
-    </div>
+    <Grid container spacing={4}>
+      <Grid container item xs={12}>
+        <Grid item xs={3}>
+          <Link to="/">
+            <Button variant="contained" color="primary">
+              Back
+            </Button>
+          </Link>
+        </Grid>
+        <Grid container item xs={6} justify="center">
+          <Typography variant="h4" component="h1">Create a Newspaper</Typography>
+        </Grid>
+        <Grid item xs={3} />
+      </Grid>
+      <Grid item xs={12}>
+        <ArticleCreator
+          onSubmit={(newArticle: Article) => addNewArticle(newArticle)}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <List>
+          {articles.map((article) => (
+            <>
+              <ListItem>
+                <ListItemText 
+                  primary={`Title: ${article.title}`} 
+                  secondary={`${article.body}`}
+                />
+              </ListItem>
+              <Divider />
+            </>
+          ))}
+        </List>
+      </Grid>
+    </Grid>
   )
 }
 
